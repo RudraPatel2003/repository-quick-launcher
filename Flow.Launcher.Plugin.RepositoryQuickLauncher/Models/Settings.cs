f8e752c82afd6069e4a72b00840a396a343df86f
Rudra Patel <patelrudra2003@gmail.com>
2025-12-09 21:46:03 -0600

    feat: Add GHA

--- Flow.Launcher.Plugin.RepositoryQuickLauncher/Models/Settings.cs
+++ Flow.Launcher.Plugin.RepositoryQuickLauncher/Models/Settings.cs
@@ -5,7 +5,6 @@ namespace Flow.Launcher.Plugin.RepositoryQuickLauncher.Models;
 public class Settings
 {
     public List<string> WindowsDirectories { get; set; } = new List<string>();
-
     public string WslDistributionName { get; set; } = "Ubuntu";
     public List<string> WslDirectories { get; set; } = new List<string>();
 }
