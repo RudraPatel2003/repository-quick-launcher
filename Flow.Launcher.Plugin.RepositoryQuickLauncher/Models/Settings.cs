945a6aa5bc5efb5b3c974e39855055171dad3ba2
Rudra Patel <patelrudra2003@gmail.com>
2026-01-01 16:21:44 -0500

    feat: Add launch command to settings

--- Flow.Launcher.Plugin.RepositoryQuickLauncher/Models/Settings.cs
+++ Flow.Launcher.Plugin.RepositoryQuickLauncher/Models/Settings.cs
@@ -4,7 +4,10 @@ namespace Flow.Launcher.Plugin.RepositoryQuickLauncher.Models;
 
 public class Settings
 {
+    public string WindowsLaunchCommand { get; set; } = "code";
     public List<string> WindowsDirectories { get; set; } = new List<string>();
+
     public string WslDistributionName { get; set; } = "Ubuntu";
+    public string WslLaunchCommand { get; set; } = "code";
     public List<string> WslDirectories { get; set; } = new List<string>();
 }
