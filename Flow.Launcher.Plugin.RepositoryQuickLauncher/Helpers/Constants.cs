692c5a209a72c73682a742a6f86187a6711734a1
Rudra Patel <patelrudra2003@gmail.com>
2026-06-11 13:19:59 -0500

    feat: Rename to FileExplorerHelper

--- Flow.Launcher.Plugin.RepositoryQuickLauncher/Helpers/Constants.cs
+++ Flow.Launcher.Plugin.RepositoryQuickLauncher/Helpers/Constants.cs
@@ -3,8 +3,12 @@ namespace Flow.Launcher.Plugin.RepositoryQuickLauncher.Helpers;
 public static class Constants
 {
     public const string IconPath = "Assets/icon.png";
+
     public const string WslPrefix = @"\\wsl$\";
+
     public const string CodeCommand = "code";
+
     public const string ExplorerCommand = "explorer";
+
     public const string ExplorerExecutable = "explorer.exe";
 }
--- Flow.Launcher.Plugin.RepositoryQuickLauncher/Helpers/RepositoryOpener.cs
+++ Flow.Launcher.Plugin.RepositoryQuickLauncher/Helpers/RepositoryOpener.cs
@@ -12,9 +12,10 @@ public static class RepositoryOpener
         CommandSetting command
     )
     {
-        if (ExplorerOpener.ShouldOpenInExplorer(repository, command))
+        if (FileExplorerHelper.ShouldOpenInFileExplorer(repository, command))
         {
-            ExplorerOpener.OpenFolder(repository, context);
+            FileExplorerHelper.OpenFolderInFileExplorer(repository, context);
+
             return;
         }
 
